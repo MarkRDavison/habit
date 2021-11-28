c00b9f289d6422e90b8a57c74b9af3962f1c9dd3
mark.davison <markdavison0@gmail.com>
2021-11-28 00:08:46 +0000

    Test

--- zeno-habit-e2e/cypress/integration/sanity/sanity-habit-web.spec.js
+++ zeno-habit-e2e/cypress/integration/sanity/sanity-habit-web.spec.js
@@ -7,14 +7,14 @@ describe('zeno habit sanity', () => {
       // so we must tell it to visit our website with the `cy.visit()` command.
       // Since we want to visit the same URL at the start of all our tests,
       // we include it in our beforeEach function so that it runs before each test
-      cy.task('log', 'CYPRESS_WEB_URL: ' + Cypress.env('WEB_URL') ?? 'https://habit-web-dev.markdavison.kiwi/')
-      cy.task('log', 'CYPRESS_BFF_URL: ' + Cypress.env('BFF_URL') ?? 'https://habit-bff-dev.markdavison.kiwi/')
-      cy.visit(Cypress.env('WEB_URL') ?? 'https://habit-web-dev.markdavison.kiwi/')
+      cy.visit(Cypress.env('WEB_URL'))
     })
 
     it('sanity go to web', () => {
       cy.contains('Zeno Habit')
       cy.contains('HOME')
+      cy.task('log', 'CYPRESS_WEB_URL: ' + Cypress.env('WEB_URL'))
+      cy.task('log', 'CYPRESS_BFF_URL: ' + Cypress.env('BFF_URL'))
     })
 
 });
